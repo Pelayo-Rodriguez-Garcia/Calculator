7287de6368348dd81d01b1cc86435920fda51c48
Pelinsquito <pelayo.r.g@gmail.com>
2019-11-21 16:53:10 +0100

    Changed to an object oriented calculator so we can extend from it

--- CalculadoraBasica.js
+++ CalculadoraBasica.js
@@ -1,8 +1,90 @@
-calculado = false;
-memoria = 0;
-expresion = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
+class Calculadora {
+    calculado = false;
+    expresion = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/;
+    memoria = 0;
+    input;
 
 
+    escribir(string) {
+        if (this.calculado && !isNaN(string)) {
+            this.limpiar();
+            document.getElementById("texto").value = string;
+
+        } else {
+            this.input = document.getElementById("texto").value
+            document.getElementById("texto").value = this.input + string;
+            this.input = document.getElementById("texto").value;
+        }
+        if (this.calculado)
+            this.calculado = false;
+    }
+
+    permitEval(string) {
+
+        if (this.expresion.test(string)) {
+            return true;
+        }
+        else {
+            return false
+        }
+    }
+
+    calcular() {
+        if (this.permitEval(document.getElementById("texto").value)) {
+            document.getElementById("texto").value = eval(document.getElementById("texto").value);
+            this.calculado = true;
+        }
+
+    }
+    limpiar() {
+        document.getElementById("texto").value = "";
+    }
+
+    sumarMemoria(e) {
+
+        if (!isNaN(document.getElementById("texto").value)) {
+            if (e == "suma") {
+                this.memoria += Number.parseInt(document.getElementById("texto").value);
+            }
+            else if (e == "resta") {
+                this.memoria -= Number.parseInt(document.getElementById("texto").value);
+            }
+
+        }
+        this.calculado = true;
+    }
+
+    mostrarMemoria() {
+        this.limpiar();
+        document.getElementById("texto").value = this.memoria;
+        this.calculado = true;
+    }
+
+
+}
+window.onload = 0;
+c = new Calculadora();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function escribir(string) {
     if (calculado && !isNaN(string)) {
         limpiar();
@@ -58,5 +140,7 @@ function mostrarMemoria() {
     document.getElementById("texto").value = memoria;
     calculado = true;
 }
+
+*/
 
 
